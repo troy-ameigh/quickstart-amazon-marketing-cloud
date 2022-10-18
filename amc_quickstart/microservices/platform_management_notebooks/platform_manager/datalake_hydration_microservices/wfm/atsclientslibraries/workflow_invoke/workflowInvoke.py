04456392f31f35aa9bc209063a9fd738054f5279
Andrew Marriott <anmarri@amazon.com>
2022-10-17 20:55:51 -0700

    Add workflow default parameter feature

--- amc_quickstart/microservices/platform_management_notebooks/platform_manager/datalake_hydration_microservices/wfm/atsclientslibraries/workflow_invoke/workflowInvoke.py
+++ amc_quickstart/microservices/platform_management_notebooks/platform_manager/datalake_hydration_microservices/wfm/atsclientslibraries/workflow_invoke/workflowInvoke.py
@@ -17,6 +17,37 @@ import boto3
 import json
 
 
+def get_workflow_default_settings(workflow_id: str, customer_id: str, table_name: str, parameters: set):
+    print(f'Override values not set for: {parameters}. Reverting to default schedule for these parameters')
+    dynamodb_client_rd= boto3.client('dynamodb')
+    wf_table_name = table_name
+
+    try:
+        response = dynamodb_client_rd.get_item(
+            TableName=wf_table_name,
+            Key = {
+                'customerId': {'S': customer_id},
+                'workflowId': {'S': workflow_id}
+            }
+        )
+        if 'Item' not in response:
+            print('ERROR: customerId and/or workflowId not found')
+            return False
+
+        default_settings = {}
+        for param in parameters:
+            try:
+                val = response['Item']['defaultSchedule']['M']['Input']['M']['payload']['M'][param]['S']
+                default_settings[param] = val
+            except Exception as e:
+                print(f'ERROR: No default value set for {e}. Set default value in the AMCWorkflows table or override in the payload')
+                return False
+        return default_settings
+    
+    except Exception as e:
+        print(e)
+        return False
+
 def invoke_workflow(workflow, TEAM_NAME, ENV):
     client = boto3.client('lambda')
     item = workflow
@@ -26,11 +57,31 @@ def invoke_workflow(workflow, TEAM_NAME, ENV):
         'payload': item['Input']['payload']
     }
 
+    check_payload_values = {
+        'timeWindowStart',
+        'timeWindowEnd',
+        'timeWindowType',
+        'workflow_executed_date',
+    }
+    payload_overrides = set(payload['payload'].keys())
+    check_payload_values -= payload_overrides
+    if len(check_payload_values) > 0:
+        default_settings = get_workflow_default_settings(
+            workflow_id=payload['payload']['workflowId'],
+            customer_id=payload['customerId'],
+            table_name=f'wfm-{TEAM_NAME}-AMCWorkflows-{ENV}',
+            parameters=check_payload_values
+            )
+        if default_settings == False: #if default settings returned an error do not invoke Lambda
+            return
+        for parameter in default_settings:
+            payload['payload'][parameter] = default_settings[parameter]
+
     response = client.invoke(
-        FunctionName=f'wfm-{TEAM_NAME}-ExecutionQueueProducer-{ENV}',
-        InvocationType='Event',
-        Payload=json.dumps(payload)
-    )
+            FunctionName=f'wfm-{TEAM_NAME}-ExecutionQueueProducer-{ENV}',
+            InvocationType='Event',
+            Payload=json.dumps(payload)
+        )
     return response
 
 def invoke_workflows(workflow_list, TEAM_NAME, ENV, customer_id):
@@ -45,6 +96,3 @@ def invoke_workflows(workflow_list, TEAM_NAME, ENV, customer_id):
         return responses
     except Exception as e:
         print(e)
-        print("Cannot add role/user to Lake Formation")
-        raise e
-    return
